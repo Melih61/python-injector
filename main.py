a1fef1c3b0a5698b77f747a34db1eae712c41cd0
maleh <107725743+Melih61@users.noreply.github.com>
2025-07-05 16:40:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,119 +1,48 @@
-import customtkinter
-from PIL import ImageTk, Image
-import io
-from tkinter import filedialog
-from tkinter import messagebox
-import urllib.request
-import tkinter as tk
-import os
-from pymem import Pymem
+from colorama import Fore, Style
+import os, getpass, platform
+import nmap
 
-def load_image(url):
-    raw_data = urllib.request.urlopen(url).read()
-    im = Image.open(io.BytesIO(raw_data)).resize((25,25))
-    image = ImageTk.PhotoImage(im)
-    return image
+PATH = os.getcwd()
+USER = getpass.getuser()
+HOST = platform.node()
 
-path = ''
-code = ''
+COMMANDS = ["nmap"]
 
-customtkinter.set_appearance_mode('Light')
-customtkinter.set_default_color_theme('blue')
+def get_cmd(user=USER, host=HOST, path=PATH):
+    return f"{Fore.RED}{user}@{host} {Fore.BLUE}{path}# {Style.RESET_ALL}"
 
-def open_file():
-    global path
-    path = filedialog.askopenfilename(filetypes = [('File', '*.py *.txt'), ('All files', '*.*')])
-    var1.set(0)
-    var2.set(1)
+class CMDS:
+    def nmap_command(target):
+        nm = nmap.PortScanner()
+        try:
+            nm.scan(target, "22-1024")
+            print(f"Scan results for {target}")
 
-def write_code():
-    def dragonwin(event):
-        x = window.winfo_pointerx() - window._offsetx
-        y = window.winfo_pointery() - window._offsety
-        window.geometry(f"+{x}+{y}")
-    def clickonwin(event):
-        window._offsetx = window.winfo_pointerx() - window.winfo_rootx()
-        window._offsety = window.winfo_pointery() - window.winfo_rooty()
-    def save():
-        global code
-        code = codebox.get(1.0, tk.END)
-        window.destroy()
-        var2.set(0)
-        var1.set(1)
-    window = customtkinter.CTk()
-    window.title('Python Injector by maleh - Write code')
-    window.overrideredirect(True)
-    window._offsetx = 0
-    window._offsety = 0
-    window.resizable(False, False)
-    window.geometry('1000x800')
-    window.configure(background='#30a4cf')
+            for host in nm.all_hosts():
+                print(f"Host: {host} ({nm[host].hostname()})")
+                print(f"OS: {nm[host]["osmatch"]}")
+                for proto in nm[host].all_protocols():
+                    print(f"Protocol: {proto}")
+                    lport = nm[host][proto].keys()
+                    for port in lport:
+                        print(f"Port: {port}\tState: {nm[host][proto][port]['state']}")
+        except Exception as e:
+            print(f"An error occurred: {e}")           
 
-    customtkinter.CTkLabel(master=window, text='Code:', text_font=('Consolas',13), text_color='white').place(y=10)
-    customtkinter.CTkButton(master=window, text='Save & Close', text_font=('Consolas',11), text_color='white', command=save).place(x=845, y=10)
-    codebox = tk.Text(master=window, font=('Consolas',11), width=120, height=150)
-    codebox.pack(pady=50)
+print(f"""{Fore.RED}
+░█▄█░█▀█░█░░░█▀▀░█░█░▀█░
+░█░█░█▀█░█░░░█▀▀░█▀█░░█░
+░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+{Style.RESET_ALL}""")
 
-    window.bind('<Button-1>',clickonwin)
-    window.bind('<B1-Motion>',dragonwin)
-    window.mainloop()
 
-def inject():
-    process = process_entry.get()
-    if process.strip() == '':
-        messagebox.showerror('Error', message='You have to enter a process')
+while True:
+    cmd = input(get_cmd())
+    cmd_split = cmd.strip().split(" ")
+    cmd_name = cmd_split[0]
+    cmd_args = cmd_split[1:]
+    if cmd_name.lower() in COMMANDS:
+        if cmd_name.lower() == "nmap":
+            CMDS.nmap_command(cmd_args[1])
     else:
-        if var1.get() == 1:
-            global code
-            try:
-                pm = Pymem(process)
-                pm.inject_python_interpreter()
-                pm.inject_python_shellcode(code)
-            except:
-                messagebox.showerror(title='Error', message='There was an error:\n1. Maybe the process does not exist\n2. Maybe there was an error while injecting code')
-        elif var2.get() == 1:
-            global path
-            if os.path.exists(path) and os.path.isfile(path):
-                with open(path, 'r') as f:
-                    code = f.read()
-                    f.close()
-                try:
-                    pm = Pymem(process)
-                    pm.inject_python_interpreter()
-                    pm.inject_python_shellcode(code)
-                except:
-                    messagebox.showerror(title='Error', message='There was an error:\n1. Maybe the process does not exist\n2. Maybe there was an error while injecting code')
-        else:
-            messagebox.showerror(title='Error', message='You have to choose one option')
-
-def change1():
-    var2.set(0)
-    var1.set(1)
-
-def change2():
-    var1.set(0)
-    var2.set(1)
-
-root = customtkinter.CTk()
-root.title('Python Injector by maleh')
-icon = load_image('https://cheatershome.netlify.app/icon_injector.ico')
-root.call('wm', 'iconphoto', root._w, icon)
-root.geometry('400x250')
-root.resizable(False, False)
-root.configure(background='#30a4cf')
-
-var1 = customtkinter.IntVar()
-var2 = customtkinter.IntVar()
-var1.set(0)
-var2.set(0)
-
-customtkinter.CTkLabel(master=root, text='Process:', text_font=('Consolas',11), text_color='white').place(relx=.5, y=30, anchor=customtkinter.CENTER)
-process_entry = customtkinter.CTkEntry(master=root, text_font=('Consolas',11), width=300)
-process_entry.place(relx=.5, y=60, anchor=customtkinter.CENTER)
-customtkinter.CTkLabel(master=root, text='Code:', text_font=('Consolas',11), text_color='white').place(relx=.5, y=100, anchor=customtkinter.CENTER)
-customtkinter.CTkButton(master=root, text='Write code', text_font=('Consolas',11),text_color='white', cursor='hand2', command=write_code).place(relx=.3, y=140, anchor=customtkinter.CENTER)
-customtkinter.CTkButton(master=root, text='Open file', text_font=('Consolas',11),text_color='white', cursor='hand2', command=open_file).place(relx=.7, y=140, anchor=customtkinter.CENTER)
-check1 = customtkinter.CTkCheckBox(master=root, variable=var1, onvalue=1, offvalue=0, text='',text_font=('Consolas',9), text_color='white', command=change1).place(relx=.3, y=170, anchor=customtkinter.CENTER)
-check1 = customtkinter.CTkCheckBox(master=root, variable=var2, onvalue=1, offvalue=0, text='',text_font=('Consolas',9), text_color='white', command=change2).place(relx=.7, y=170, anchor=customtkinter.CENTER)
-customtkinter.CTkButton(master=root, text='Inject', text_font=('Consolas',15), text_color='white', cursor='hand2', command=inject).place(relx=.5, y=210, anchor=customtkinter.CENTER)
-root.mainloop()
+        os.system(cmd)
